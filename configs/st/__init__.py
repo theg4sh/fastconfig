4843e672cc344c967f024bea47afba61e3ec0d2d
Alexandr Kondratev <theg4sh@gmail.com>
2018-01-07 18:23:26 -0500

    Added st terminal with Terminess Powerline font

---
+++ configs/st/__init__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from modules.PackageManager import *
+from modules.PathThirdparty import *
+import subprocess as sp
+
+def requires(cfg):
+	cfg.addPackageDependency("fontconfig-devel", "libX11-devel",
+            "libXft-devel", mgrtype=PackageManager.MANAGER_RPM)
+
+def configure(cfg):
+	cfg.addLinkFile(PathThirdparty("st", name="config.h").abspath(), "config.h");
+
+def postinstall(cfg):
+	make = "make -C {}".format(PathThirdparty("st").absdir())
+	sys.stdout.write(make)
+	sp.check_output(make, shell=True)
+	print(" Done")
+	print("root permission required for:")
+	print()
+	print("  {} install".format(make))
+	print()
