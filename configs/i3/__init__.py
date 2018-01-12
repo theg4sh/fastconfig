3571e75487c971faf2742d308aa453bd2e761ea3
Alexandr Kondratev <theg4sh@gmail.com>
2018-01-12 11:45:55 -0500

    i3's config: colorful separator, new symbol, Powerline's Terminus for dmenu.

--- configs/i3/__init__.py
+++ configs/i3/__init__.py
@@ -1,8 +1,11 @@
 from modules.PackageManager import *
 
 def requires(cfg):
-	cfg.addPackageDependency("xorg-x11-server-Xorg", "i3", mgrtype=PackageManager.MANAGER_RPM)
-	cfg.addPackageDependency("xserver-xorg", "i3", mgrtype=PackageManager.MANAGER_DPKG)
+	cfg.addPackageDependency("xorg-x11-server-Xorg", "i3",
+		"pulseaudio-utils", mgrtype=PackageManager.MANAGER_RPM)
+	cfg.addPackageDependency("xserver-xorg", "i3",
+		"pulseaudio-utils", mgrtype=PackageManager.MANAGER_DPKG)
+	cfg.addPackageDependency("fontawesome-fonts")
 
 def configure(cfg):
 	cfg.addLinkPath("~/.config/i3", "i3")
