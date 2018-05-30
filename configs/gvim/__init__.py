1c631ab4ea488927359120ebf5699984c71259e9
Alexandr Kondratev <ak@cnord.ru>
2018-05-30 21:10:07 +0300

    TypeScript syntax highlight added

--- configs/gvim/__init__.py
+++ configs/gvim/__init__.py
@@ -1,9 +1,18 @@
 from modules.PathConfig import *
 from modules.PathThirdparty import *
 
+def requires(cfg):
+    try:
+        cfg.addPackageDependecy("vim-youcompleteme", mgrtype=PackageManager.MANAGER_DPKG);
+    except:
+        pass
+
 def configure(cfg):
-    cfg.addLinkFile("~/.vimrc", "vimrc")
     cfg.addLinkPath("~/.vim", "vim")
+    cfg.addLinkFile("~/.vimrc", "vimrc")
+
     cfg.addLinkPath(PathConfig(cfg.getConfigName(), name="vim/pack/plugins/start/vim-go"), PathThirdparty("vim-go"))
+    cfg.addLinkPath(PathConfig(cfg.getConfigName(), name="vim/pack/plugins/start/typescript-vim"), PathThirdparty("typescript-vim"))
+
     cfg.addLinkFile(PathConfig(cfg.getConfigName(), name="vim/syntax/python.vim"), PathThirdparty("vim-python-syntax", name="syntax/python.vim"))
 
